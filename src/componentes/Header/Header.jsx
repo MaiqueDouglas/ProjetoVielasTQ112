cecc7e5dbfb7f3efd838f70920687fc9e20d9d49
Maique Douglas <rmaique762@gmail.com>
2024-04-04 12:29:52 -0300

    Criação do componente Modal

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from 'react';
 import './Header.css';
 import usuario from "../../assets/usuario.png"
+import LoginModal from "../LoginModal/LoginModal";
 
 function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div>
       <div className="container">
@@ -13,10 +23,11 @@ function Header() {
         </div>
         <h1>VIELAS</h1>
      
-        <div className="login">
+        <div className="login" onClick={openModal}>
           <span>Login</span>
           <img src={usuario} alt="User Icon" />   
         </div>
+        {isModalOpen && <LoginModal onClose={closeModal} />}
       </div>
    </div>
   );
